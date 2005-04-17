b2b14674edef59e32fbcc9166c9276656d3110da
wuttke <>
2005-04-17 14:51:34 +0000

    Initial import.

---
+++ TinyRadius/TinyRadius/src/org/tinyradius/util/RadiusException.java
@@ -0,0 +1,23 @@
+/**
+ * $Id$
+ * Created on 10.04.2005
+ * @author Matthias Wuttke
+ * @version $Revision$
+ */
+package org.tinyradius.util;
+
+/**
+ * An exception which occurs on Radius protocol errors like
+ * invalid packets or malformed attributes.
+ */
+public class RadiusException extends Exception {
+
+	/**
+	 * Constructs a RadiusException with a message.
+	 * @param message error message
+	 */
+	public RadiusException(String message) {
+		super(message);
+	}
+	
+}
